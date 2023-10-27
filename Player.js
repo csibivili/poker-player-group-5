2dff419f8421ac1e4d8da7e38c8e31d059e22dc8
babityrichard <babity.richard@gmail.com>
2023-10-27 16:25:44 +0200

    fix: fixes + removed extra bet

--- Player.js
+++ Player.js
@@ -14,19 +14,25 @@ class Player {
       const minimumRaise = game.minimumRaise();
       const hasPocketPair = me.hasPocketPair();
 
+      console.log("AFTER_INITIATIONS");
+
       console.log("MINIMUM_RAISE", minimumRaise);
       console.log("STACK_VALUE", stackValue);
       console.log("HAS_POCKET_PAIR", hasPocketPair);
 
       // raise on good cards
-      if (scoreNumber > 3 && stackValue > minimumRaise * 1.5) {
+      if (
+        scoreNumber > 3 &&
+        stackValue > minimumRaise * 1.5 &&
+        !hasPocketPair
+      ) {
         bet(minimumRaise * 1.5);
+      } else {
+        bet(game.minimumRaise());
       }
-
-      bet(game.minimumRaise());
+      console.log("END_OF_TRY");
     } catch (error) {
       console.error(error);
-      bet(0);
     }
   }
 
