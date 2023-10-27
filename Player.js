ca69107aa844767b97ffa8d90af0f6692685920b
babityrichard <babity.richard@gmail.com>
2023-10-27 16:10:58 +0200

    feat: added good cards minRaise

--- Player.js
+++ Player.js
@@ -1,23 +1,30 @@
-const GameState = require('./src/GameState');
+const GameState = require("./src/GameState");
 
 class Player {
   static get VERSION() {
-    return '0.1';
+    return "0.1";
   }
 
   static betRequest(gameState, bet) {
     try {
       var game = new GameState(gameState);
+      const scoreNumber = game.me().score();
+      const me = game.me();
+      const stackValue = me.stack();
+      const minimumRaise = game.minimumRaise();
+
+      if (scoreNumber > 10 && stackValue > minimumRaise * 1.5) {
+        bet(minimumRaise * 1.5);
+      }
+
       bet(game.minimumRaise());
     } catch (error) {
-      console.log(error)
-      bet(0)
+      console.error(error);
+      bet(0);
     }
   }
 
-  static showdown(gameState) {
-  }
+  static showdown(gameState) {}
 }
 
 module.exports = Player;
-
